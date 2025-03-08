f33e68381a0cbf40ccfdf4fe28b46a6df82ca24f
thnalmeida <thaynale@gmail.com>
2025-03-08 20:59:47 -0300

    Adição da função sortear amigo

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ let listaAmigos = [];
 
 function adicionarAmigo(){
     let nomeAmigo = document.querySelector('input').value;
-
     //Implemente uma validação para garantir que o campo não esteja vazio.
     // Se estiver vazio, exiba um alerta com a mensagem de erro: 
     // "Por favor, insira um nome."
@@ -16,11 +15,38 @@ function adicionarAmigo(){
     } else{
         listaAmigos.push(nomeAmigo); //para adicionar o nome digitado do amigo na lista de amigos
         console.log(listaAmigos);
-        limparCampo();
+        limparCampo(); //chamo a função para que o input fique limpo para o usuario digitar outro nome
     }
 }
 
 function limparCampo() {
     nomeAmigo = document.querySelector('input');
     nomeAmigo.value = '';
+}
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function atualizarLista(){
+    exibirTextoNaTela('h1', listaAmigos);
+}
+
+//Escreva uma função que selecione aleatoriamente um dos nomes armazenados no array amigos.
+// Use Math.random() e Math.floor() para obter um índice aleatório.
+function sortearAmigo(){
+    //Validar que há amigos disponíveis: Antes de sortear, verificar se o array amigos não está vazio.
+    if(listaAmigos.lenght === 0){
+        alert("Digite o nome dos seus amigos.")
+    }else{
+        //Gerar um índice aleatório: Usar Math.random() e Math.floor() para selecionar um índice aleatório do array.
+        let indice = Math.floor(Math.random() * listaAmigos.length);
+        //Obter o nome sorteado: Utilizar o índice aleatório para acessar o nome correspondente no array.
+        amigoSorteado = listaAmigos[indice];
+    }
+    //Mostrar o resultado: Atualizar o conteúdo do elemento de resultado usando document.getElementById() e innerHTML para exibir o amigo sorteado.
+    exibirTextoNaTela('h1', `O seu amigo secreto é: ${amigoSorteado}`)
+
+
 }
